e4cd582d3ddaeb60c8b84f4a164fab1a166bd56c
nikita_mullovsky <mullovskii@gmail.com>
2018-10-16 11:10:44 +0300

    money

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,6 +1,23 @@
 class Currency < ApplicationRecord
 	belongs_to :country
 
+	after_create :money
+
+	def money
+		# require 'money'
+		# # 10.00 USD
+		# require 'eu_central_bank'
+		# eu_bank = EuCentralBank.new
+		# Money.default_bank = eu_bank
+		# money1 = Money.new(10)
+		# money1.bank # eu_bank
+		# eu_bank.update_rates
+		# eu_bank.exchange(100, "CAD", "USD")
+		# money = Money.us_dollar(1).exchange_to("RUB")
+		# money = Money.new(1000, "EUR").exchange_to("USD")
+		puts money.bank
+	end
+
 	def to_usd_price(amount)
 		usd_rate = self.usd_rate #заменить на поставщика курсов
 		amount/usd_rate
